e99c3c284a6c539445ec537bdb83ed5dbf282c7d
Jeet Chawda <jchawda332@gmail.com>
2023-08-06 22:48:39 +0530

    Desktop - technology page completed

--- src/Pages/Technology.jsx
+++ src/Pages/Technology.jsx
@@ -32,22 +32,59 @@ const Technology = () => {
       <div className="technoContainer text-white">
         <div></div>
         <div>
-          <h3>
-            <span>03</span>SPACE LAUNCH 101
-          </h3>
-          <div>
-            <a href="">1</a>
-            <a href="">2</a>
-            <a href="">3</a>
+          <div className="mt-10 mb-4 ml-2">
+            <h3 className="font-barlowCon text-2xl tracking-widest font-light">
+              <span className="font-bold text-zinc-600 mr-2">03</span>SPACE
+              LAUNCH 101
+            </h3>
           </div>
-          <div>
-            <h4>THE TERMINOLOGY...</h4>
-            <h1>{data[currentInfo].term}</h1>
-            <p>{data[currentInfo].description}</p>
+          <div className="flex justify-between items-center">
+            <div>
+              <div className="flex gap-x-12">
+                <div className="flex flex-col text-2xl gap-y-9">
+                  <button
+                    href=""
+                    onClick={() => setCurrentIfo(0)}
+                    className={`border-[1px] w-fit rounded-full px-[23px] py-[14px] ${
+                      currentInfo + 1 == 1 ? "bg-white text-black" : ""
+                    }`}
+                  >
+                    1
+                  </button>
+                  <button
+                    onClick={() => setCurrentIfo(1)}
+                    className={`border-[1px] w-fit rounded-full px-[23px] py-[14px] ${
+                      currentInfo + 1 == 2 ? "bg-white text-black" : ""
+                    }`}
+                  >
+                    2
+                  </button>
+                  <button
+                    onClick={() => setCurrentIfo(2)}
+                    className={`border-[1px] w-fit rounded-full px-[23px] py-[14px] ${
+                      currentInfo + 1 == 3 ? "bg-white text-black" : ""
+                    }`}
+                  >
+                    3
+                  </button>
+                </div>
+                <div>
+                  <h4 className="font-barlowCon text-[#D0D6F9]">
+                    THE TERMINOLOGY...
+                  </h4>
+                  <h1 className="font-bellefair text-5xl mt-3 mb-3">
+                    {data[currentInfo].term}
+                  </h1>
+                  <p className="font-barlow w-[450px] text-[#D0D6F9] leading-8">
+                    {data[currentInfo].description}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img src={data[currentInfo].image} alt="" />
+            </div>
           </div>
-        </div>
-        <div>
-          <img src={data[currentInfo].image} alt="" />
         </div>
       </div>
     </div>
